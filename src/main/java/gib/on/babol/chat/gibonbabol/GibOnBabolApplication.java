eb4dcefd16feb3c27fc5b6142b377132a980aafc
lange-m <ml@michallange.com>
2018-09-17 21:09:09 +0200

    initial

---
+++ src/main/java/gib/on/babol/chat/gibonbabol/GibOnBabolApplication.java
@@ -0,0 +1,12 @@
+package gib.on.babol.chat.gibonbabol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GibOnBabolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GibOnBabolApplication.class, args);
+	}
+}
---
+++ src/test/java/gib/on/babol/chat/gibonbabol/GibOnBabolApplicationTests.java
@@ -0,0 +1,16 @@
+package gib.on.babol.chat.gibonbabol;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GibOnBabolApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
